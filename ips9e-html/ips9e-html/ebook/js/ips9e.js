9a23e1c290cc52e313bec395fa786e3489434849
dustydog10 <meaderoberts@comcast.net>
2016-04-30 17:06:37 -0700

    Building in supplemental windows and styling

--- ips9e-html/ips9e-html/ebook/js/ips9e.js
+++ ips9e-html/ips9e-html/ebook/js/ips9e.js
@@ -66,9 +66,22 @@ var Player_subtype = Player_manuscript_type.extend({
 	var link_handler = new LinkHandler();
 
 	// Add custom JS code below. Anything that can be done after the
-	// page displays should be done here.
+        // page displays should be done here.
+
+        //*****************DEVLIONS***********************
+
+	var chapter_number = $('#manuscript').attr("data-chapter-number");
+
+        // remove all default DF click/mouseup events from image tags
+	$('img').unbind();
 
 
+        //TABLES
+        // Adjust table widths according to the percentage specified in the "data-attr" attribute
+	$('div[data-type="table"]').each(function () {
+	    var twidth = $(this).attr('data-attr') * 6.75; // assuming 675px width of main content (not including page margins)
+	    $(this).find('table').css('width', twidth);
+	});
 
     // all figures displaying on main page
 	// figures aligned left need flagged as such (use a non digfir flag)
@@ -84,14 +97,6 @@ var Player_subtype = Player_manuscript_type.extend({
 	// $('[data-block_type="h1"] [data-caption-compass]').removeAttr( "data-type" );
 	$('[data-type="figure"] > img').removeAttr("data-layout-width");
 
-	//now change the size of the image by a percentage
-	//$('[data-block_type="h1"] [data-caption-compass]').each(function() {
-	//	var scale = .20;
-	//	image = $(this).find('img') //update... this will find caption images too. 
-	//	var h = image.height() * scale;
-	//	var w = image.width() * scale;
-	//	image.css({ height: h, width: w });
-	//});
 
 	//if image needs caption "West" (to the left of table) flip caption and image so that caption comes first
 	//and place extra div around img
@@ -103,49 +108,141 @@ var Player_subtype = Player_manuscript_type.extend({
 	$('[data-block_type="h1"] [data-caption-compass*="W"] > img').wrap("<div class='compassImg'></div>");
 
 
-    // common code for all popup requests - size/position of popup
-    var pop_content = function(url, w, h) {
-        var path = window.location.pathname;
-	    var newWin = window.open(url,'_blank','width='+w+',height='+h+',menubar=0,location=0,scrollbars=yes','');
+        //All the links to exercises/examples/figures/tables/chapters/sections/pages shall be done here
+        //popup requests to pages/sections/chapters will use the default digfir functionality
+        //popup requests to exercises/examples/figures/tables will use the "pop_content()" function defined below
+        //The reasoning: ebook navigation between sections has to follow a strict protocol due to Macmillan's Launchpad tool. Therefore we must
+        //use the functions provided by digfir to popup pages/sections/chapters. But with other items (ie. exercises) there is
+        //no such restriction, and Macmillan would like more flexibility (controlling the size of the popup windows, how many, etc.).
+
+        //chapter references
+        //assumes a specific xml filename was used by vendor in the <link> tag
+        //assumes link to the first section in the chapter
+	$('a[href^="scc9e-ch"][href$=".xml"]').each(function () {
+	    var newhref = $(this).attr('href').replace(/.*ch0?(\d+).*/i, "scc9e_ch$1_1.html");
+	    //certain chapters have parts as the first section, so in those cases, adjust the url to link to the second section
+	    if (/ch(1|10|17|21)_/.test(newhref)) { newhref = newhref.replace(/(.*)_1.html/, "$1_2.html"); }
+	    $(this).attr('href', newhref);
+	});
+
+        //section references...  
+        //difficult to automate. We will see how the vendor codes this in the XML.
+        //May be a manual thing? Not sure how vendor could get right... they won't know the html filename to link to
+
+        //page references
+        //No need to do anything. XML supplied by vendor should be good. Linking functionality taken care of by digfir
+
+        //turn in text references from <a> to <spans> (avoiding digfir default linking)
+	$('a[href^="table_"], a[href^="figure_"], a[href^="exercise_"], a[href^="example_"]').each(function () {
+	    var text = $(this).html();
+	    $(this).replaceWith("<span data_href=" + $(this).attr("href") + ">" + text + "</span>");
+	});
+
+
+        // SUPPLEMENTAL WINDOW CLICK EVENTS
+        //popup requests to supplemental windows for examples/exercises/figures/table will use this function.
+        //Input is URL, width/height of window
+	var pop_content = function (url, w, h) {
+	    var path = window.location.pathname;
+	    var newWin = window.open(url, '_blank', 'width=' + w + ',height=' + h + ',menubar=0,location=0,scrollbars=yes', '');
 	    newWin.moveTo(150, 150);
-    };
+	};
+
+        //Table references
+        // Number in caption of table needs linked, along with title
+	$('[data-block_type="TABLE"] [data-type="table_caption"]').click(function () {
+	    //need table number
+	    var filename = $(this).closest('[data-type="table"]').attr("data-filename");
+	    var supp_win = "asset/ch" + chapter_number + "/supp_wins/tables/" + filename;
+	    pop_content(supp_win, "1020px", "500px");
+	});
+        // add link on table references in the text
+	$('span[data_href^="table_"]').click(function () {
+	    var filename = $(this).attr('data_href');
+	    //var ch = filename.replace(/table_(\d+).*/i, "$1");
+	    var supp_win = "asset/ch" + chapter_number + "/supp_wins/tables/" + filename;
+	    pop_content(supp_win, "1015px", "700px");
+	});
 
 
-////EXAMPLE LINKS
-//        // add link to popup on the example number
-//        $('[data-block_type="EXP"] > [data-type="box_inner"] > [data-block_type="EXP-N"]').click(function() {
-//            var exnum = $(this).find('p').text().replace(/EXAMPLE ([\d\.]+)/i, "$1");
-//            var supp_win = exnum.replace(/(\d+)\.(\d+)/, "asset/ch$1/supp_wins/examples/example_$1_$2.html");
-//	    pop_content(supp_win, "1020px", "500px");
-//        });
+        //Links to example
+        // The example box has a title and number, add link on the example number
+	$('[data-block_type="EXP"] > [data-type="box_inner"] > [data-block_type="EXP-T"]').click(function () {
+	    //need chapter number, grab from a <p> id.
+	    var ch = $(this).attr('id').replace(/.*ch0?(\d+).*/i, "$1");
+	    var exnum = $(this).find('p [data-block_type="EXP-N-ri"]').text().replace(/EXAMPLE ([\d]+)/i, "$1");
+	    var supp_win = "asset/ch" + ch + "/supp_wins/examples/example_" + ch + "_" + exnum + ".html";
+	    pop_content(supp_win, "1020px", "500px");
+	});
+        // add link on example reference in the text
+	$('span[data_href^="example_"]').click(function () {
+	    var filename = $(this).attr('data_href');
+	    //var ch = filename.replace(/example_(\d+).*/i, "$1");
+	    var supp_win = "asset/ch" + chapter_number + "/supp_wins/examples/" + filename;
+	    pop_content(supp_win, "1020px", "500px");
+	});
 
 
-////NUMBERED FIGURE LINKS
-//	// add link on the figure image so it popup (
-//	$('[data-block_type="h1"] [data-caption-compass]  > .compassImg img').click(function() {
-//            var fignum = $(this).attr('src').replace(/fig_([\d_]+)/i, "$1");
-//            var supp_win = fignum.replace(/.*0?(\d+)_0?(\d+)\.jpg/, "asset/ch$1/supp_wins/figures/figure_$1_$2.html");
-//	    pop_content(supp_win, "1015px", "700px");
-//        });
-//        // add link on the figure image to popup 
-//	$('[data-block_type="h1"] [data-caption-compass]  > img').click(function() {
-//            var fignum = $(this).attr('src').replace(/fig_([\d_]+)/i, "$1");
-//            var supp_win = fignum.replace(/.*0?(\d+)_0?(\d+)\.jpg/, "asset/ch$1/supp_wins/figures/figure_$1_$2.html");
-//	    pop_content(supp_win, "1015px", "700px");
-//        });
-//	// add link on the figure number to popup
-//	$('[data-block_type="h1"] [data-caption-compass] [data-block_type="FG-N-ri"]').click(function() {
-//            var fignum = $(this).text().replace(/FIGURE ([\d\.]+)/i, "$1");
-//            var supp_win = fignum.replace(/(\d+)\.(\d+)/, "asset/ch$1/supp_wins/figures/figure_$1_$2.html");
-//	    pop_content(supp_win, "1015px", "700px");
-//        });
-	
-	
+        //Exercises
+        //add link on exercise titles
+        //"now it's your turn" exercises found throughout sections
+	$('[data-type="question"] [data-block_type="BX2-QUE-N-ri"]').click(function () {
+	    //need question number	               
+	    var exernum = $(this).text().replace(/ *(\d+)\.(\d+).*/i, "exercise_$1_$2.html");
+	    var supp_win = "asset/ch" + exernum.replace(/exercise_(\d+)_.*/, "$1") + "/supp_wins/exercises/" + exernum;
+	    pop_content(supp_win, "1020px", "500px");
+	});
+        //these are at end of chapter
+	$('[data-type="question"] [data-block_type="CR-X-NL-N-ri"]').click(function () {
+	    //need question number	               
+	    var exernum = $(this).text().replace(/ *(\d+)\.(\d+).*/i, "exercise_$1_$2.html");
+	    var supp_win = "asset/ch" + exernum.replace(/exercise_(\d+)_.*/, "$1") + "/supp_wins/exercises/" + exernum;
+	    pop_content(supp_win, "1020px", "500px");
+	});
+        // add link on exercise references in the text
+	$('span[data_href^="exercise_"]').click(function () {
+	    var filename = $(this).attr('data_href');
+	    var ch = filename.replace(/exercise_(\d+).*/i, "$1");
+	    var supp_win = "asset/ch" + ch + "/supp_wins/exercises/" + filename;
+	    pop_content(supp_win, "1015px", "700px");
+	});
 
 
+        //NUMBERED FIGURE LINKS
+        //// add link on the figure image
+        //    $('[data-caption-compass]  > .compassImg img').click(function() {
+        //        var fignum = $(this).attr('src').replace(/fig_([\d_]+)/i, "$1");
+        //        var supp_win = fignum.replace(/.*0?(\d+)_0?(\d+)\.jpg/, "asset/ch$1/supp_wins/figures/figure_$1_$2.html");
+        //        pop_content(supp_win, "1015px", "700px");
+        //    });
+        //// add link on the figure image (sometimes img is not inside "compass"... occurs when caption in default place under figure)
+        //    $('[data-caption-compass]  > img').click(function() {
+        //        var fignum = $(this).attr('src').replace(/fig_([\d_]+)/i, "$1");
+        //        var supp_win = fignum.replace(/.*0?(\d+)_0?(\d+)\.jpg/, "asset/ch$1/supp_wins/figures/figure_$1_$2.html");
+        //        pop_content(supp_win, "1015px", "700px");
+        //    });
 
+        // add link on the figure number in caption
+	$('[data-caption-compass] [data-block_type="FG-N-ri"]').click(function () {
+	    var fignum = $(this).text().replace(/FIGURE ([\d\.]+)/i, "$1");
+	    var supp_win = fignum.replace(/(\d+)\.(\d+)/, "asset/ch$1/supp_wins/figures/figure_$1_$2.html");
+	    pop_content(supp_win, "1015px", "700px");
+	});
+    // add link on figure references in the text	                
+	$('span[data_href^="figure_"]').click(function () {
+	    var filename = $(this).attr('data_href');
+	    var ch = filename.replace(/figure_(\d+).*/i, "$1");
+	    var supp_win = "asset/ch" + ch + "/supp_wins/figures/" + filename;
+	    pop_content(supp_win, "1015px", "700px");
+	});
 
-
+    // add link on the figure image
+	$('[data-type="figure"] img').click(function () {
+	    var filename = $(this).closest('[data-type="figure"]').attr("data-filename");
+	    //var supp_win = filename.replace(/(.*0?(\d+)_0?\d+\.html)/, "asset/ch$2/supp_wins/figures/$1");
+	    var supp_win = "asset/ch" + chapter_number + "/supp_wins/figures/" + filename;
+	    pop_content(supp_win, "1015px", "700px");
+	});
 
 
 	
--- ips9e-html/ips9e-html/xml/addDataFilename.pl
+++ ips9e-html/ips9e-html/xml/addDataFilename.pl
@@ -11,13 +11,13 @@ use strict;
 binmode(STDIN, ":utf8");
 binmode(STDOUT, ":utf8");
 
-# my $baseXMLfile = "scc9e-ch01.xml"; #-------------------------> identify digfir XML file here
-# my $baseXMLfile = "scc9e-ch02.xml"; #-------------------------> identify digfir XML file here
-# my $baseXMLfile = "scc9e-ch03.xml"; #-------------------------> identify digfir XML file here
-# my $baseXMLfile = "scc9e-ch04.xml"; #-------------------------> identify digfir XML file here
-# my $baseXMLfile = "scc9e-ch05.xml"; #-------------------------> identify digfir XML file here
-# my $baseXMLfile = "scc9e-ch06.xml"; #-------------------------> identify digfir XML file here
-my $baseXMLfile = "scc9e-ch03.xml"; #-------------------------> identify digfir XML file here
+my $baseXMLfile = "ips9e-ch01.xml"; #-------------------------> identify digfir XML file here
+# my $baseXMLfile = "ips9e-ch02.xml"; #-------------------------> identify digfir XML file here
+# my $baseXMLfile = "ips9e-ch03.xml"; #-------------------------> identify digfir XML file here
+# my $baseXMLfile = "ips9e-ch04.xml"; #-------------------------> identify digfir XML file here
+# my $baseXMLfile = "ips9e-ch05.xml"; #-------------------------> identify digfir XML file here
+# my $baseXMLfile = "ips9e-ch06.xml"; #-------------------------> identify digfir XML file here
+
 my $datahrefXML_file = "${baseXMLfile}_datahref.xml";
 
 
@@ -45,26 +45,26 @@ print "chapter: $chapter\n";
 
 
 # examples
-$xml_contents =~ s/(<box[^>]*block_type="EXP"[^>]*)(>\s*<p[^>]*>\s*<phrase[^>]*block_type="EXP-N-ri">EXAMPLE (\d+))/$1 data-filename="example_${chapter}_$3.html"$2/gs;
+$xml_contents =~ s/(<box[^>]*block_type="EXP"[^>]*)(>\s*<p[^>]* block_type="EXP-N">EXAMPLE (\d+)\.(\d+))/$1 data-filename="example_$3_$4.html"$2/gs;
 
 # tables (numbered)
-$xml_contents =~ s/(<table[^>]*block_type="TABLE"[^>]*)(>.*?<phrase block_type="TBN-N-ri">TABLE (\d+)\.(\d+))/$1 data-filename="table_${chapter}_$4.html"$2/gs;
+$xml_contents =~ s/(<table[^>]*block_type="TABLE"[^>]*)(>.*?<phrase block_type="TBN-N">TABLE (\d+)\.(\d+))/$1 data-filename="table_${chapter}_$4.html"$2/gs;
 
 
 # figures (numbered)
 $xml_contents =~ s/(<figure[^>]*block_type="FIGURE"[^>]*)(>.*?<phrase block_type="FG-N-ri">Figure (\d+)\.(\d+))/$1 data-filename="figure_${chapter}_$4.html"$2/gs;
 
 # figures (unnumbered)
-$xml_contents =~ s/(<figure[^>]*block_type="UN-FIGURE"[^>]*)(>.*?<image[^>]*src="[^"]*\/([^"]*)\.jpg")/$1 data-filename="${chapter}_$3.html"$2/gs;
+$xml_contents =~ s/(<figure[^>]*block_type="UN-FIGURE"[^>]*)(>.*?<image[^>]*src="asset\/ch\d+\/(.*?)\.jpg")/$1 data-filename="$3.html"$2/gs;
 
-# exercises (Now it's your turn)
-$xml_contents =~ s/(<question[^>]*)(>\s*<p [^>]*>\s*<phrase block_type="BX2-QUE-N-ri">\s*(\d+)\.(\d+))/$1 block_type="exercise_${chapter}_$4.html"$2/gs;
+# exercises
+$xml_contents =~ s/(<question[^>]*)(>\s*<p [^>]*>\s*<phrase block_type="EXR-QUE-N-ri">\s*(\d+)\.(\d+))/$1 block_type="exercise_${chapter}_$4.html"$2/gs;
 
-# exercises (the rest)
-$xml_contents =~ s/(<question[^>]*)(>\s*<p [^>]*>\s*<phrase block_type="CR-X-NL-N-ri">\s*(\d+)\.(\d+))/$1 block_type="exercise_${chapter}_$4.html"$2/gs;
+# exercises (Exercises)
+$xml_contents =~ s/(<question[^>]*)(>\s*<p [^>]*>\s*<phrase block_type="SR-XH">\s*(\d+)\.(\d+))/$1 block_type="exercise_${chapter}_$4.html"$2/gs;
 
 # exercises (with icon in front)
-$xml_contents =~ s/(<question[^>]*)(>\s*<p [^>]*>\s*<image [^>]*>\s*<phrase block_type="CR-X-NL-N-ri">\s*(\d+)\.(\d+))/$1 block_type="exercise_${chapter}_$4.html"$2/gs;
+$xml_contents =~ s/(<question[^>]*)(>\s*<p [^>]*>\s*<image [^>]*>\s*<phrase block_type="SR-XH">\s*(\d+)\.(\d+))/$1 block_type="exercise_${chapter}_$4.html"$2/gs;
 
 
 
